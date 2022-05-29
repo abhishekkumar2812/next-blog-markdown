eb347eb981324d06c44b7ac0814688b250dcadf7
unknown <vibrant.abhishek@gmail.com>
2022-05-29 14:05:38 +0530

    next verrsion

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -1,66 +1,75 @@
 import { FunctionComponent } from "react";
-import fs from 'fs';
+import fs from "fs";
 import matter from "gray-matter";
-import styles from '../../styles/article.module.css';
+import styles from "../../styles/article.module.css";
 import { ArticleInfo } from "../../interfaces/article";
 import Markdown from "../../component/markdown";
+import Image from "next/image";
 
 interface IProps {
-    article: ArticleInfo;
+  article: ArticleInfo;
 }
 
 const Article: FunctionComponent<IProps> = ({ article }) => {
-	return <div className={styles.article}>
-        <div className={styles.thumbnail}>
-            <img src={article.meta.thumbnail} />
-
-            <div className={styles.title}>
-                <h1>{article.meta.title}</h1>
-            </div>
+  return (
+    <div className={styles.article}>
+      <div className={styles.thumbnail}>
+        {/* <Image src={article.meta.thumbnail} alt="thumbnanil" /> */}
+        <Image
+          src={article.meta.thumbnail}
+          unoptimized
+          priority
+          width="100%"
+          height="100%"
+          layout="fill"
+          alt="banner"
+        />
+        <div className={styles.title}>
+          <h1>{article.meta.title}</h1>
         </div>
+      </div>
 
-        <div className={styles.content}>
-            <Markdown content={article.content} />
-        </div>
-	</div>
-}
+      <div className={styles.content}>
+        <Markdown content={article.content} />
+      </div>
+    </div>
+  );
+};
 
 export async function getStaticProps({ ...ctx }) {
-    const { slug } = ctx.params;
+  const { slug } = ctx.params;
 
-    const content = fs
-        .readFileSync(`uploads/${slug}.md`)
-        .toString();
+  const content = fs.readFileSync(`uploads/${slug}.md`).toString();
 
-    const info = matter(content);
+  const info = matter(content);
 
-    const article = {
-        meta: {
-            ...info.data,
-            slug
-        },
-        content: info.content
-    }
+  const article = {
+    meta: {
+      ...info.data,
+      slug,
+    },
+    content: info.content,
+  };
 
-    return {
-        props: {
-            article: article
-        }
-    }
+  return {
+    props: {
+      article: article,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-    const files = fs.readdirSync("uploads");
-    const paths = files.map(file => ({
-        params: {
-            slug: file.split('.')[0]
-        }
-    }))
-    
-    return {
-        paths,
-        fallback: false,
-    }
+  const files = fs.readdirSync("uploads");
+  const paths = files.map((file) => ({
+    params: {
+      slug: file.split(".")[0],
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default Article;
